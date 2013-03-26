1e54792de984673277a3c36940fa8b262a67611d
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-03-26 16:14:57 +0100

    s

--- web/index.php
+++ web/index.php
@@ -12,12 +12,12 @@ function formulaire($today)
 	echo "
 	<table width='100%'>
 		<tr>
-			<td span='3'><div id='graphUvmByAllSite' style='min-width: 400px; height: 400px; margin: 0 auto'></div></td>
+			<td span='3' align='center' ><div id='graphUvmByAllSite' style='min-width: 400px; height: 400px; margin: 0 auto'></div></td>
 		</tr>
 		<tr>
-			<td width='33%'><div id='graphUvmBySiteBagnolet' style='min-width: 400px; height: 400px; margin: 0 auto'></div></td>
-			<td width='33%'><div id='graphUvmBySiteSophia' style='min-width: 400px; height: 400px; margin: 0 auto'></div></td>
-			<td width='33%'><div id='graphUvmBySiteMontsouris' style='min-width: 400px; height: 400px; margin: 0 auto'></div></td>
+			<td width='33%' align='center'><div id='graphUvmBySiteBagnolet' style='min-width: 400px; height: 400px; margin: 0'></div></td>
+			<td width='33%' align='center'><div id='graphUvmBySiteSophia' style='min-width: 400px; height: 400px; margin: 0'></div></td>
+			<td width='33%' align='center'><div id='graphUvmBySiteMontsouris' style='min-width: 400px; height: 400px; margin: 0'></div></td>
 		</tr>
 	</table>
 	
