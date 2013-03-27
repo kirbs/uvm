4eb8e174c47a20459c9c8ec0fcc4cb3592f015b2
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-03-27 10:29:13 +0100

    ajout de index2.php

---
+++ index2.php
@@ -0,0 +1,17 @@
+<?php
+
+include("header.html");
+?>
+
+
+
+<table width='100%'>
+	<tr>
+		<td span='3' align='center' ><div id='graphUvmByAllSite' style='min-width: 400px; height: 400px; margin: 0 auto'></div></td>
+	</tr>
+	<tr>
+		<td width='33%' align='center'><div id='graphUvmBySiteBagnolet' style='min-width: 100px; height: 400px; margin: 0'></div></td>
+		<td width='33%' align='center'><div id='graphUvmBySiteSophia' style='min-width: 400px; height: 400px; margin: 0'></div></td>
+		<td width='33%' align='center'><div id='graphUvmBySiteMontsouris' style='min-width: 400px; height: 400px; margin: 0'></div></td>
+	</tr>
+</table>
