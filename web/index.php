0300175344e3591d57cc937b889e4d82bb9421dc
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-03-28 15:18:25 +0100

    s

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@ function formulaire($today)
 	
 	<table width='100%'>
 		<tr>
-			<td span='3' align='center' ><div id='graphUvmByAllSite' style='min-width: 521px; height: 200px; margin: 0 auto'></div></td>
+			<td span='3' align='center' ><div id='graphUvmByAllSite' style='min-width: 600px; height: 400px; margin: 0 auto'></div></td>
 		</tr>
 		<tr>
 			<td width='33%' align='center'><div id='graphUvmBySiteBagnolet' style='min-width: 400px; height: 521px; margin: 0; display: none'></div></td>
