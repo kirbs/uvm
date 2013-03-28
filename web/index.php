86574ca23846bc8ca50109430835ce9811096685
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-03-28 15:52:31 +0100

    graph population

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@ function formulaire($today)
 	
 	<table width='100%' border=1>
 		<tr>
-			<td width='33%' align='center'></td>
+			<td width='33%' align='center'><div id='population'></div></td>
 			<td width='33%' align='center' ><div id='graphUvmByAllSite' style='min-width: 700px; height: 400px; margin: 0 auto'></div></td>
 			<td width='33%' align='center'></td>
 		</tr>
