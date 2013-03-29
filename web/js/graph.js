0662d70d51e16d12daff41e4564a0292956be826
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-03-29 14:23:01 +0100

    s

--- web/js/graph.js
+++ web/js/graph.js
@@ -302,7 +302,7 @@ $(function() {
                 	color: 'rgba(223, 83, 83, .5)',
                 	data: output_population['HT2']
             	},{
-                	name: 'MTSs',
+                	name: 'MTS',
                 	color: 'rgba(119, 83, 83, .5)',
                 	data: output_population['Montsouris']
             	}]
