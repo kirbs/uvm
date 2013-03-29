f3810aff9582ea5c9b0af33f92c5e8f133b6ec08
kovac <akovac@akovac.akovac.lan>
2013-03-29 16:13:47 +0100

    f

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -10,7 +10,7 @@ $(function() {
         $( "#datepicker" ).datetimepicker({ dateFormat: "yy-mm-dd", timeFormat: "hh:mm:ss", hourGrid: 4, minuteGrid: 10, });
         $( "#duration" ).timepicker({ timeFormat: "hh:mm:ss", hourGrid: 4, minuteGrid: 10  });
         $( "#choice_date" ).datepicker({ dateFormat: "yy-mm-dd" });
-        $( "#choice_date_click" ).datepicker({ beforeShowDay: "13-04-01", dateFormat: "yy-mm-dd" , altField: "#choice_date_click_field"});
+        $( "#choice_date_click" ).datepicker({  dateFormat: "yy-mm-dd" , altField: "#choice_date_click_field"});
         $( "#choice_date_to" ).datepicker({ dateFormat: "yy-mm-dd" });
 
 	});
