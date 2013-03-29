0e4b401fa3d60211b1abb2a729cf2ddcd3d69a2c
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-03-29 12:33:23 +0100

    d

--- web/js/graph.js
+++ web/js/graph.js
@@ -300,7 +300,7 @@ $(function() {
             	series: [{
                 	name: 'All Sites',
                 	color: 'rgba(223, 83, 83, .5)',
-                	data: output_population[1]
+                	data: output_population[0]
             	}]
         	});
     	});
