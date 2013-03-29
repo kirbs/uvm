dbfa4bb83d4af466f898880d1fe345f7ca76fbf7
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-03-29 11:48:00 +0100

    s

--- web/js/graph.js
+++ web/js/graph.js
@@ -76,7 +76,7 @@ $(function() {
 	                        color: '#000000',
 	                        connectorColor: '#000000',
     	                    formatter: function() {
-	                            return '<b>'+ this.point.name +'</b>: '+ this.point.y +' uVM';
+	                            return '<b>'+ this.point.name +'</b>: '+ this.point.y +' VM';
 	                        }
 	                    }
 	                }
