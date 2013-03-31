90312e6cac849dcdba5dd849f4aa757eac4b52fb
kovac <akovac@akovac.akovac.lan>
2013-03-31 03:56:11 +0200

    s

--- web/js/graph.js
+++ web/js/graph.js
@@ -177,7 +177,7 @@ $(function() {
 	    $.getJSON('api.php?command=getListPfsMereUvmBySite&site=Montsouris', function(data) {
 			chart = new Highcharts.Chart({
 	            chart: {
-	                renderTo: 'graphUvmBySiteMont souris',
+	                renderTo: 'graphUvmBySiteMontsouris',
 	                plotBackgroundColor: null,
 	                plotBorderWidth: null,
 	                plotShadow: false
@@ -499,7 +499,54 @@ $(function() {
 		});		
 		
 		
-		
+
+
+
+
+
+
+	    $.getJSON('api.php?command=getListPfsMereUvmBySiteBack', function(data) {
+	    	$.each(data[2], function(i,val){
+	    		console.log(i);
+	    		console.log(val);
+				/*
+				 chart = new Highcharts.Chart({
+		            chart: {
+		                renderTo: 'graphUvmBySiteMontsouris',
+		                plotBackgroundColor: null,
+		                plotBorderWidth: null,
+		                plotShadow: false
+		            },
+		            title: {
+		                text: 'Pourcentage d\'uVM par PFS (Montsouris)'
+		            },
+		            tooltip: {
+		                    pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+		                percentageDecimals: 2
+		            },
+		            plotOptions: {
+		                pie: {
+		                    allowPointSelect: true,
+		                    cursor: 'pointer',
+		                    dataLabels: {
+		                        enabled: true,
+		                        color: '#000000',
+		                        connectorColor: '#000000',
+	    	                    formatter: function() {
+		                            return '<b>'+ this.point.name +'</b>: '+ this.point.y +' uVM';
+		                        }
+		                    }
+		                }
+		            },
+		            series: [{
+		                type: 'pie',
+		                name: 'uVM',
+		                data: data
+		            }]
+				}); */
+			});
+		});
+		// <!-- Fin GRAPH uVM ALL SITE By Site Montsouris -->	
 		
 		
 		
