d7f6c8167ca892d0ae4fb8cd7e16bedae668f129
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-02 16:22:14 +0200

    s

--- web/js/graph.js
+++ web/js/graph.js
@@ -210,7 +210,7 @@ $(function() {
 	                    cursor: 'pointer',
 	                    dataLabels: {
 	                        enabled: true,
-	                        color: '#00ff00',
+	                        color: '#000000',
 	                        connectorColor: '#000000',
     	                    formatter: function() {
 	                            return '<b>'+ this.point.name +'</b>: '+ this.point.y +' uVM';
