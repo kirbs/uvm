3192ceb3b34cafe43914829fff208a87d192864d
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-02 11:54:15 +0200

    s

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -4,20 +4,26 @@ $(function() {
     //var chart;
     $(document).ready(function() {
 		
-		function availableDates(date) {
+
 			var availableDates = new Array();
 			$.getJSON('api.php?command=getListDate', function(data) {
 	        	availableDates = data;
-	        	console.log(availableDates);
+	        	
+				function availableDates(date) {	        	
+					//availableDates = ["2013-3-25","2013-3-28","2013-4-1"];
+					ymd = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+					if ($.inArray(ymd, availableDates) == -1) {
+						return [false, "", "aucun traitement ce jour"];
+					} else {
+						return [true,"","OK"];
+					}
+				};
+	        	$( "#choice_date_click" ).datepicker({  minDate: '-1M' , maxDate : 0 , dateFormat: "mm-dd-yy" ,altField: "#choice_date_click_field", beforeShowDay: availableDates});
+	     	});
+	        	
+	        	//console.log(availableDates);
 			
-				//availableDates = ["2013-3-25","2013-3-28","2013-4-1"];
-				ymd = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
-				if ($.inArray(ymd, availableDates) == -1) {
-					return [false, "", "aucun traitement ce jour"];
-				} else {
-					return [true,"","OK"];
-				}
-			});
+
 			//availableDates = ["2013-3-25","2013-3-28","2013-4-1"];
 			//ymd = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
 			//if ($.inArray(ymd, availableDates) == -1) {
@@ -25,7 +31,7 @@ $(function() {
 			//} else {
 			//	return [true,"","OK"];
 			//}
-		}
+		//}
 
         //<!-- DEFINITION DU CALENDRIER -->
 
@@ -33,7 +39,7 @@ $(function() {
         $( "#duration" ).timepicker({ timeFormat: "hh:mm:ss", hourGrid: 4, minuteGrid: 10  });
         $( "#choice_date" ).datepicker({ dateFormat: "yy-mm-dd" });
         //$( "#choice_date_click" ).datepicker({  dateFormat: "yy-mm-dd" ,beforeShowDay: disableSpecificDates ,altField: "#choice_date_click_field"});
-        $( "#choice_date_click" ).datepicker({  minDate: '-1M' , maxDate : 0 , dateFormat: "mm-dd-yy" ,altField: "#choice_date_click_field", beforeShowDay: availableDates});
+        //$( "#choice_date_click" ).datepicker({  minDate: '-1M' , maxDate : 0 , dateFormat: "mm-dd-yy" ,altField: "#choice_date_click_field", beforeShowDay: availableDates});
         $( "#choice_date_to" ).datepicker({ dateFormat: "yy-mm-dd" });
 
 	});
