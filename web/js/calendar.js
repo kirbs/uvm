d25bf6a4cfa034b86624872f072190c2e3135dc9
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-02 11:03:18 +0200

    d

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -19,26 +19,33 @@ $(function() {
 			return [true];
 		}
 
+		function availableDates(date) {
+      		//var dat = $.datepicker.formatDate("yy-mm-dd", d);
+			//natDays = ["2013-03-25","2013-03-29"];
+
+			//for (var i=0, c=natDays.length; i<c; i++)
+ 			//	if (dat >= natDays[i][0] && dat<=natDays[i][1]) {
+  			//		return [true, "", "test"];
+          	//	}
+			//	else  {
+			//		return [false, ""];
+			//	}
+			unavailableDates = ["25-3-2013","28-4-2013","1-4-2013"];
+			dmy = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+			if ($.inArray(dmy, unavailableDates) == -1) {
+				return [true, ""];
+			} else {
+				return [false,"","Unavailable"];
+			}
+		}
+
         <!-- DEFINITION DU CALENDRIER -->
 
         $( "#datepicker" ).datetimepicker({ dateFormat: "yy-mm-dd", timeFormat: "hh:mm:ss", hourGrid: 4, minuteGrid: 10, });
         $( "#duration" ).timepicker({ timeFormat: "hh:mm:ss", hourGrid: 4, minuteGrid: 10  });
         $( "#choice_date" ).datepicker({ dateFormat: "yy-mm-dd" });
         //$( "#choice_date_click" ).datepicker({  dateFormat: "yy-mm-dd" ,beforeShowDay: disableSpecificDates ,altField: "#choice_date_click_field"});
-        $( "#choice_date_click" ).datepicker({  minDate: '-1M' , maxDate : 0 , dateFormat: "mm-dd-yy" ,altField: "#choice_date_click_field", beforeShowDay:function(d) {
-      		var dat = $.datepicker.formatDate("yy-mm-dd", d);
-			natDays = ["2013-03-25","2013-03-29"];
-
-			for (var i=0, c=natDays.length; i<c; i++)
- 				if (dat >= natDays[i][0] && dat<=natDays[i][1]) {
-  					return [true, "", "test"];
-          		}
-				else  {
-					return [false, ""];
-				}
-
-          	}
-      	});
+        $( "#choice_date_click" ).datepicker({  minDate: '-1M' , maxDate : 0 , dateFormat: "mm-dd-yy" ,altField: "#choice_date_click_field", beforeShowDay: availableDates});
         $( "#choice_date_to" ).datepicker({ dateFormat: "yy-mm-dd" });
 
 	});
