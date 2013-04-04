eb33a7b15a190757361fe4c2f9efd2e83af33a4d
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-04 14:30:14 +0200

    z

--- web/js/datatable.js
+++ web/js/datatable.js
@@ -17,15 +17,29 @@ YUI().use( "sortable", "calendar", "datatype-date", "datatable", "io", function 
    	});
    	
 
+    Y.io('api.php?command=ggetListPfsMere',{
+    	on : {
+    		success : function(tx,r) {
+    			var json = Y.JSON.parse(r.responseText);
+    			
+				var $select = $('#datas');
+				$.each(json, function(i,val){
+					$select.append($('<option />', {
+						value : val[i+1], text : val[i+1]
+					}));
+				});
+    		}
+    	}
+   	});	
 	
-	var array = [{'1':'Name'},{'2':'Age'}, {'3':'Gender'}];
+	//var array = [{'1':'Name'},{'2':'Age'}, {'3':'Gender'}];
 	
-	var $select = $('#datas');
-	$.each(array, function(i,val){
-		$select.append($('<option />', 
-		{value : (i+1), text : val[i+1]
-	}));
-	});
+	//var $select = $('#datas');
+	//$.each(json, function(i,val){
+	//	$select.append($('<option />', {
+	//		value : (i+1), text : val[i+1]
+	//	}));
+	//});
 
 
 
