42f34e2fa146597321e3881df64eb28f6debb516
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-05 09:57:10 +0200

    s

--- web/index.php
+++ web/index.php
@@ -28,7 +28,7 @@ function formulaire($today)
 						<option value='pfs'>PFS</option>
 						<option value='xen'>Serveur Xen</option>
 					</select> -->
-					<select id='datas' name=choix>
+					<select id='datas' name=pfs>
 						<option value='--'>-- Choix de la PFS --</option>
 						<option value='0'>- ALL -</option>
 					</select>
