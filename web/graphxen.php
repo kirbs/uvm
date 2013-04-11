ef5b19ff039c609cc90ed21912694f7398af15d8
kovac <akovac@akovac.akovac.lan>
2013-04-11 22:19:02 +0200

    s

--- web/graphxen.php
+++ web/graphxen.php
@@ -3,5 +3,5 @@ include("header.html");
 ?>
 	<script src="js/graphxen.js"></script>
 
-	<div id='Capacity_Alpha'></div>
-	<div id='Capacity_Dave'></div>
+	<!-- <div id='Capacity_Alpha'></div>
+	<div id='Capacity_Dave'></div> -->
