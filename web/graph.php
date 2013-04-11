a7b2ddea241a281b94e95914796bd64ea0548eb8
kovac <akovac@akovac.akovac.lan>
2013-04-11 22:02:26 +0200

    s

--- web/graph.php
+++ web/graph.php
@@ -24,6 +24,7 @@ include("header.html");
 	</table>
 	<div id='graphUvmBySiteImmeuble Gambetta' style='height: 500px; min-width: 500px'></div>
 	<div id='graphUvmBySitenone' style='height: 500px; min-width: 500px'></div>
+	<div id='graphUvmBySiteHT2' style='height: 500px; min-width: 500px'></div>
 
 	<div id='graphUvmXenByHT2' style='height: 500px; min-width: 500px'></div>
 	<div id='graphUvmXenByBagnolet' style='height: 500px; min-width: 500px'></div>
