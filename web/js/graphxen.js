9fdade19dfcfb7ab0322ce6670099b5b37a8312e
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-12 10:22:01 +0200

    s

--- web/js/graphxen.js
+++ web/js/graphxen.js
@@ -16,7 +16,7 @@ $(function() {
 	                text: 'Stacked column chart'
 	            },
 	            xAxis: {
-	                categories: data[0][0]
+	                categories: ["xen0stratus001.cloud.s1.p.fti.net","xen0stratus002.cloud.s1.p.fti.net","xen0stratus003.cloud.s1.p.fti.net","xen0stratus004.cloud.s1.p.fti.net","xen0stratus005.cloud.s1.p.fti.net","xen0stratus006.cloud.s1.p.fti.net","xen0stratus007.cloud.s1.p.fti.net","xen0stratus008.cloud.s1.p.fti.net","xen0stratus009.cloud.s1.p.fti.net","xen0stratus010.cloud.s1.p.fti.net","xen0stratus201.cloud.b1.p.fti.net","xen0stratus202.cloud.b1.p.fti.net","xen0stratus203.cloud.b1.p.fti.net","xen0stratus204.cloud.b1.p.fti.net","xen0stratus205.cloud.b1.p.fti.net","xen0stratus206.cloud.b1.p.fti.net","xen0stratus207.cloud.b1.p.fti.net","xen0stratus208.cloud.b1.p.fti.net","xen0stratus209.cloud.b1.p.fti.net","xen0stratus210.cloud.b1.p.fti.net"]
 	                //['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
 	            },
 	            yAxis: {
@@ -61,11 +61,11 @@ $(function() {
 	            },
 	            series: [{
 	                name: 'uMem',
-	                data: data[1][0]
+	                data: ["58","58","58","58","58","58","58","104","58","104","58","58","58","58","58","58","58","104","58","108"]
 	                //[5, 3, 4, 7, 2]
 	            }, {
 	                name: 'uDisk',
-	                data: data[2][0]
+	                data: ["64","65","65","65","74","65","65","70","65","70","65","65","65","65","65","65","65","67","65","68"]
 	                //[2, 2, 3, 2, 1]
 	            }]
 	        });
