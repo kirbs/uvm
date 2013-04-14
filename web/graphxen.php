4ef56c47e8bee57ede4ed47a3dd4ff8c59c11cd7
kovac <akovac@akovac.akovac.lan>
2013-04-14 05:29:40 +0200

    z

--- web/graphxen.php
+++ web/graphxen.php
@@ -10,8 +10,15 @@ include("config.inc");
 
 
 	$ReqListBulle = mysql_query("SELECT distinct(bulle) FROM SrvXen order by bulle ASC");
+	echo '<table>';
+	echo '<tr>';
+	$cpt = 0;
 	while($array = mysql_fetch_array($ReqListBulle))
 	{
-		echo "<div id='Capacity_$array[bulle]' style='width: 50%;'></div><br><br>";
+		$cpt = $cpt + 1;
+		echo "<td><div id='Capacity_$array[bulle]' style='width: 50%;'></div></td>";
+		if (($cpt % 2) == 0)
+			echo '</tr><tr>';
 	}
+	echo "</tr></table>";
 ?>
