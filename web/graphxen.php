daf41162af935cf7bf1213f6761ce3d1183a7b78
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-16 11:47:48 +0200

    s

--- web/graphxen.php
+++ web/graphxen.php
@@ -13,9 +13,10 @@ include("config.inc");
 	//echo '<table>';
 	//echo "<tr><td width='50%'></td><td width='50%'></td></tr>";
 	//$cpt = 0;
+	echo "<table>";
 	while($array = mysql_fetch_array($ReqListBulle))
 	{
-		echo "<div id='Capacity_$array[bulle]' style='width: 80%;'></div><br><br>";
+		echo "<tr><td align='center'><div id='Capacity_$array[bulle]' style='width: 80%;'></div></td></tr>";
 		//$cpt = $cpt + 1;
 		//if (($cpt % 2) != 0)
 		//	echo '<tr>';
