40cd0df4fc4e3bf439be1afef0895f7a3edd1ac7
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-18 16:30:45 +0200

    s

--- web/js/autocomplete.js
+++ web/js/autocomplete.js
@@ -10,29 +10,7 @@ $(function() {
      	});
      	
      	
-		
-		
-		$("#demo1").jstree({ 
-			"json_data" : {
-				"data" : [
-					{ 
-						"data" : "A node", 
-						"metadata" : { id : 23 },
-						"children" : [ "Child 1", "A Child 2" ]
-					},
-					{ 
-						"attr" : { "id" : "li.node.id1" }, 
-						"data" : { 
-							"title" : "Long format demo", 
-							"attr" : { "href" : "#" } 
-						} 
-					}
-				]
-			},
-			"plugins" : [ "themes", "json_data", "ui" ]
-		}).bind("select_node.jstree", function (e, data) { 
-			alert(data.rslt.obj.data("id"));
-			});
+
 
 
 	});
