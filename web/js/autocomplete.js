de58e6a2c6a3e50620a2ebb229e3979b0f7a6af0
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-18 12:10:31 +0200

    q

--- web/js/autocomplete.js
+++ web/js/autocomplete.js
@@ -1,12 +1,36 @@
-$(function() {
+YUI().use('autocomplete', 'autocomplete-filters', 'autocomplete-highlighters', function (Y) {
+	
+	Y.io('api.php?command=getListVM',{
+    	on : {
+    		success : function(tx,r) {
+    			var json = Y.JSON.parse(r.responseText);
+    		
+ 
+				Y.one('#tabs').plug(Y.Plugin.AutoComplete, {
+					resultFilters    : 'phraseMatch',
+				    resultHighlighter: 'phraseMatch',
+				    source           : json
+				});	
+    		}
+    	}
+   	});
+});	
 
-    $(document).ready(function() {
-		
-		var availableDates = new Array();
-		$.getJSON('api.php?command=getListVM', function(data) {
-			$( "#tags" ).autocomplete({
-				source: data
-			});
-     	});
-	});
-});
+
+
+
+
+
+
+//$(function() {
+//
+//    $(document).ready(function() {
+//		
+//		var availableDates = new Array();
+//		$.getJSON('api.php?command=getListVM', function(data) {
+//			$( "#tags" ).autocomplete({
+//				source: data
+//			});
+//     	});
+//	});
+//});
