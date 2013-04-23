9760af465479564615569daffab2850cd239a63f
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2013-04-23 14:57:51 +0200

    s

--- web/index.php
+++ web/index.php
@@ -53,9 +53,9 @@ function formulaire($today)
 					</td>
 				</tr>
 			</table>
-			</form>";
-			//TreeTable_uvm_by_pfs_mere($today);
-			echo "
+			</form>
+			
+			
 			
 			<div id='template' class='yui3-skin-sam dt-example yui3-g'> <!-- You need this skin class -->
 		    	<div class='yui3-u-1-3' id='TableAllUvm'></div>
@@ -69,7 +69,7 @@ function formulaire($today)
 			</div>
 		</div>
 	</div>";
-
+	////TreeTable_uvm_by_pfs_mere($today);
 }
 
 function affiche_liste_pfs($choix,$date)
